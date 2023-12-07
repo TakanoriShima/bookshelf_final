4a0464d6e1d60c529eb9daf2dfd9bc829bdc7fb7
TakanoriShima <quark2galaxy@gmail.com>
2023-12-07 09:14:58 +0000

    sqlファイル修正

--- dump.sql
+++ dump.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.35, for Linux (x86_64)
 --
--- Host: localhost    Database: bookshelf
+-- Host: localhost    Database: bookshelf_final
 -- ------------------------------------------------------
 -- Server version	5.7.35
 
@@ -18,11 +18,6 @@
 --
 -- Table structure for table `books`
 --
-DROP DATABASE IF EXISTS `bookshelf_final`;
-
-CREATE DATABASE `bookshelf_final` DEFAULT CHARACTER SET UTF8;
-
-USE `bookshelf_final`;
 
 DROP TABLE IF EXISTS `books`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -34,8 +29,11 @@ CREATE TABLE `books` (
   `status` varchar(10) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `books_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,8 +42,36 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (2,'詳しい解説付き！HTML5','./images/item_book_4.jpg','finished','2021-11-09 09:39:36','pass'),(3,'実践Webアプリケーション開発','./images/item_book_3.jpg','reading','2021-11-09 09:41:05','pass'),(4,'実践で学ぶSEO入門','./images/item_book_2.jpg','reading','2021-11-09 09:41:56','pass'),(5,'初めてのプログラミング','./images/item_book_1.jpg','pending','2021-11-09 09:42:34','pass'),(7,'コミック','./uploads/91UvGWmqdkL.jpg','finished','2021-11-15 04:10:20','pass'),(8,'PHP（下）','./uploads/41vb-rC5lOL._AC_SY200_.jpg','unread','2021-11-15 04:33:00','pass'),(9,'PHP（上）','./uploads/41zPn5lFAgL._AC_SY200_.jpg','pending','2021-11-17 07:22:45','pass'),(10,'実践　PHP+MySQL入門','./uploads/41N2XZC06FL._AC_SY200_.jpg','unread','2021-11-20 05:06:17','pass'),(13,'Numbers 久保','./uploads/100000009003176744_10203_001.jpg','reading','2021-12-09 03:35:42','pass'),(14,'人を動かす','./uploads/41LVhY-YlhL._AC_SY200_.jpg','finished','2021-12-16 00:47:57','hoge');
+INSERT INTO `books` VALUES (2,'詳しい解説付き！HTML5','./images/item_book_4.jpg','finished','2021-11-09 09:39:36','pass',1),(3,'実践Webアプリケーション開発','./images/item_book_3.jpg','reading','2021-11-09 09:41:05','pass',1),(4,'実践で学ぶSEO入門','./images/item_book_2.jpg','finished','2021-11-09 09:41:56','pass',1),(5,'初めてのプログラミング','./images/item_book_1.jpg','pending','2021-11-09 09:42:34','pass',1),(7,'コミック','./uploads/91UvGWmqdkL.jpg','finished','2021-11-15 04:10:20','pass',1),(8,'PHP（下）','./uploads/41vb-rC5lOL._AC_SY200_.jpg','unread','2021-11-15 04:33:00','pass',1),(9,'PHP（上）','./uploads/41zPn5lFAgL._AC_SY200_.jpg','pending','2021-11-17 07:22:45','pass',1),(10,'実践　PHP+MySQL入門','./uploads/41N2XZC06FL._AC_SY200_.jpg','unread','2021-11-20 05:06:17','pass',1),(13,'Numbers 久保','./uploads/100000009003176744_10203_001.jpg','pending','2021-12-09 03:35:42','pass',1),(14,'人を動かす','./uploads/41LVhY-YlhL._AC_SY200_.jpg','pending','2021-12-16 00:47:57','pass',1),(15,'JavaScriptコードレシピ集','./uploads/41BRrm0lJYL._AC_SY200_.jpg','reading','2021-12-16 01:05:08','pass',1),(17,'Music','./uploads/2052921912-481x680.jpg','unread','2021-12-25 06:22:25','pass',3);
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'島尚徳','shima@gmail.com','shima','2021-12-25 05:40:06'),(3,'山田太郎','yamada@gmail.com','yamada','2021-12-25 06:18:08');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -57,16 +83,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-16 10:07:17
-
-create table users (
-  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL UNIQUE,
-  `password` varchar(255) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-UPDATE users SET id=1;
-ALTER TABLE books ADD user_id int NOT NULL;
-UPDATE books SET user_id=1;
-ALTER TABLE books ADD FOREIGN KEY(user_id) REFERENCES users(id);
+-- Dump completed on 2021-12-25 15:26:55
